3255fbec3840657c521946483d6978df3a9bf436
Gargaj <gargaj@conspiracy.hu>
2013-08-15 00:21:24 +0200

    Fix BBCode to be case insens + honor user settings

--- include_generic/libbb.php
+++ include_generic/libbb.php
@@ -16,7 +16,7 @@ function bbencode_parse_tag($message,$tag,$openCode,$closeCode)
   // First things first: If there aren't any "[quote]" strings in the message, we don't
   // need to process it at all.
   
-  if (strpos(strtolower($message), "[".$tag."]")===false)
+  if (stripos(strtolower($message), "[".$tag."]")===false)
   {
     return $message;  
   }
@@ -27,7 +27,7 @@ function bbencode_parse_tag($message,$tag,$openCode,$closeCode)
   $curr_pos = 0;
   while ($curr_pos!==false && ($curr_pos < strlen($message)))
   { 
-    $curr_pos = strpos($message, "[", $curr_pos);
+    $curr_pos = stripos($message, "[", $curr_pos);
   
     // If not found, $curr_pos will be 0, and the loop will end.
     if ($curr_pos!==false)
@@ -100,17 +100,20 @@ function bbencode_parse_tag($message,$tag,$openCode,$closeCode)
  
 function bbencode( $text )
 {
-  $text = preg_replace("/\[b\](.*?)\[\/b\]/s","<b>$1</b>",$text);
-  $text = preg_replace("/\[i\](.*?)\[\/i\]/s","<i>$1</i>",$text);
-  $text = preg_replace("/\[u\](.*?)\[\/u\]/s","<u>$1</u>",$text);
+  $text = preg_replace("/\[b\](.*?)\[\/b\]/si","<b>$1</b>",$text);
+  $text = preg_replace("/\[i\](.*?)\[\/i\]/si","<i>$1</i>",$text);
+  $text = preg_replace("/\[u\](.*?)\[\/u\]/si","<u>$1</u>",$text);
   $text = bbencode_parse_tag($text,"quote","<section class=\"bbs_quote\"><b>Quote:</b><blockquote>","</blockquote></section>");
   $text = bbencode_parse_tag($text,"code","<section class=\"bbs_code\"><b>Code:</b><pre>","</pre></section>");
-  $text = preg_replace("/\[list\](.*?)\[\/list\]/s","<ul>$1</ul>",$text);
-  $text = preg_replace("/\[list=(.*?)\](.*?)\[\/list\]/s","<ol type='$1'>$2</ol>",$text);
+  $text = preg_replace("/\[list\](.*?)\[\/list\]/si","<ul>$1</ul>",$text);
+  $text = preg_replace("/\[list=(.*?)\](.*?)\[\/list\]/si","<ol type='$1'>$2</ol>",$text);
   $text = preg_replace("/\[\*\](.*)[\r\n]/","<li>$1</li>",$text);
-  $text = preg_replace("/\[url\](.*?)\[\/url\]/","<a href='$1'>$1</a>",$text);
-  $text = preg_replace("/\[url=(.*?)\](.*?)\[\/url\]/","<a href='$1'>$2</a>",$text);
-  $text = preg_replace("/\[img\](.*?)\[\/img\]/","<img src='$1' class='bbimage' alt='BB Image'/>",$text);
+  $text = preg_replace("/\[url\](.*?)\[\/url\]/i","<a href='$1'>$1</a>",$text);
+  $text = preg_replace("/\[url=(.*?)\](.*?)\[\/url\]/i","<a href='$1'>$2</a>",$text);
+  if (get_setting("displayimages"))
+    $text = preg_replace("/\[img\](.*?)\[\/img\]/i","<img src='$1' class='bbimage' alt='BB Image'/>",$text);
+  else
+    $text = preg_replace("/\[img\](.*?)\[\/img\]/i","<a href='$1'>[image disabled]</a>",$text);
 //  $text = preg_replace("/\s([a-zA-Z0-9]+:\/\/[a-zA-Z\.\-_0-9\+\/]+)/","<a href='$1' target='_blank'>$1</a>",$text);
 //  $text = preg_replace("/([a-zA-Z\.\-_0-9\+]+@[a-zA-Z\.\-_0-9\+]+)/","<a href='mailto:$1' target='_blank'>$1</a>",$text);
 //  $text = "<p>".preg_replace("/[\r\n]{2,}/","</p>\n\n<p>",$text)."</p>";
