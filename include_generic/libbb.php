389a5008563001342791b7e54a04a3354f83b279
Gargaj <gargaj@conspiracy.hu>
2013-08-20 14:50:07 +0200

    Worksafe override

--- include_generic/libbb.php
+++ include_generic/libbb.php
@@ -110,7 +110,7 @@ function bbencode( $text )
   $text = preg_replace("/\[\*\](.*)[\r\n]/","<li>$1</li>",$text);
   $text = preg_replace("/\[url\](.*?)\[\/url\]/si","<a href='$1'>$1</a>",$text);
   $text = preg_replace("/\[url=(.*?)\](.*?)\[\/url\]/si","<a href='$1'>$2</a>",$text);
-  if (get_setting("displayimages"))
+  if (get_setting("displayimages") || $_GET["forceimages"])
     $text = preg_replace("/\[img\](.*?)\[\/img\]/i","<img src='$1' class='bbimage' alt='BB Image'/>",$text);
   else
     $text = preg_replace("/\[img\](.*?)\[\/img\]/i","<a href='$1'>[image disabled]</a>",$text);
