f8f600181f6a940b4d9fc25249b4952070f4ad4e
Gargaj <gargaj@conspiracy.hu>
2013-08-22 02:18:58 +0200

    Obey limit

--- export/last-prod-comments.rss.php
+++ export/last-prod-comments.rss.php
@@ -2,6 +2,10 @@
 require_once("../bootstrap.inc.php");
 require_once( POUET_ROOT_LOCAL . "/include_pouet/pouet-rss.php");
 
+$limit = $_GET["howmany"] ? $_GET["howmany"] : 10;
+$limit = min($limit,25);
+$limit = max($limit,5);
+
 $s = new BM_Query("comments");
 $s->AddField("comments.id");
 $s->AddField("comments.quand");
@@ -10,7 +14,7 @@ $s->attach(array("comments"=>"which"),array("prods as prod"=>"id"));
 $s->attach(array("comments"=>"who"),array("users as user"=>"id"));
 $s->AddWhere(sprintf_esc("comments.which = %d",$_GET["prod"]));
 $s->AddOrder("comments.quand DESC");
-$s->SetLimit("20");
+$s->SetLimit($limit);
 $data = $s->perform();
 
 $rss = new PouetRSS();
