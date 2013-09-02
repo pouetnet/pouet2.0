74477a3b1c2c7c8c32c1248cba0d41335fea2ef3
Arthur McBain <mcbain.asm@gmail.com>
2013-09-02 05:30:16 -0700

    Added some code to the end of script.js, and styles necessary for it to look good to style.css, for displaying a list of 'editor' buttons for bb-code to the right of the textarea on BBS and prod pages. This code is based on the 'Pouet bb-codeizer for winnars!' user script but refactored for slightly cleaner code, and formatting that better matches what already exists in script.js. It does not include the automatic 'by line' insertion code and other non-editor-essential code from the user script.

--- script.js
+++ script.js
@@ -338,3 +338,173 @@ function Youtubify( e )
   });
 }
 
+
+
+
+
+(function ()
+{
+  "use strict"; // See http://daringfireball.net/2010/07/improved_regex_for_matching_urls
+  var textarea, buttons, url = /\b((?:[a-z][\w-]+:(?:\/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}\/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))/i;
+
+  // Replaces the selected text in a textarea with the given text optionally highlighting a subset
+  // of the replacement text based on insets from the start and end of the replacement text.
+  function replaceText (text, sinset, einset)
+  {
+    var start = textarea.selectionStart, end = textarea.selectionEnd;
+
+    textarea.value = textarea.value.substring(0, start) + text + textarea.value.substring(end);
+
+    if (typeof sinset === "number" && typeof einset === "number") {
+      textarea.setSelectionRange(start + sinset, start + text.length - einset);
+    }
+    else
+    {
+      textarea.setSelectionRange(start + text.length, start + text.length);
+    }
+    textarea.focus();
+  }
+
+  function getText ()
+  {
+    return textarea.value.substring(textarea.selectionStart, textarea.selectionEnd);
+  }
+
+  // Puts proper bb-code around selected text and places the cursor after it,
+  // or inserts some bb-code-surrounded dummy text and selects the dummy text.
+  function simpleEdit ()
+  {
+    var text = getText(), start = this.code[0], end = this.code[1];
+
+    if (text) {
+      replaceText(start + text + end);
+    }
+    else {
+      replaceText(start + this.text + end, start.length, end.length);
+    }
+  }
+
+  function listEdit ()
+  {
+    var text = getText(), start = this.code[0], end = this.code[1];
+
+    if (text)
+    {
+      text = start + text.replace(/(?:^|\n)([^\n]+)/g, "[*]$1\n") + end;
+      replaceText(text, text.length + 1, 0);
+    }
+    else
+    {
+      text = start + this.text + end;
+      replaceText(text, start.length + 3, end.length + 1);
+    }
+  }
+
+  buttons = [ {
+    name: "Bold",
+    code: [ "[b]", "[/b]" ],
+    text: "bold text"
+  }, {
+    name: "Italic",
+    code: [ "[i]", "[/i]" ],
+    text: "italic text"
+  }, {
+    name: "Link",
+    code: [ "[url]", "[/url]" ],
+    text: "http://example.com"
+  }, {
+    "class": "link_alt",
+    name: "Link with alternate text",
+    text: "http://example.com",
+    smpl: "linky",
+    click: function ()
+    {
+      var text = getText(), start = "[url=", end = "[/url]";
+
+      if (text)
+      {
+        if (url.test(text))
+        {
+          replaceText(start + text + "]" + this.smpl + end, start.length + text.length + 1, end.length);
+        }
+        else
+        {
+          replaceText(start + this.text + "]" + text + end, start.length, end.length + text.length + 1);
+        }
+      }
+      else
+      {
+        text = start + this.text + "]" + this.smpl + end;
+        replaceText(text, start.length, 1 + this.smpl.length + end.length);
+      }
+    }
+  }, {
+    name: "Email",
+    code: [ "[email]", "[/email]" ],
+    text: "example@example.com"
+  }, {
+    name: "Image",
+    code: [ "[img]", "[/img]" ],
+    text: "http://example.com/image.png"
+  }, {
+    name: "Quote",
+    code: [ "[quote]", "[/quote]" ],
+    text: "quoted text"
+  }, {
+    name: "Code",
+    code: [ "[code]", "[/code]" ],
+    text: "// code comment"
+  }, {
+    name: "Bulleted list",
+    code: [ "[list]\n", "[/list]" ],
+    text: "[*]list item\n",
+    click: listEdit
+  }, {
+    name: "Alphabet list",
+    code: [ "[list=A]\n", "[/list]" ],
+    text: "[*]A is for alligator\n",
+    click: listEdit
+  }, {
+    name: "Numbered list",
+    code: [ "[list=1]\n", "[/list]" ],
+    text: "[*]list item\n",
+    click: listEdit
+  } ];
+
+  document.observe("dom:loaded", function ()
+  {
+    textarea = $("message") || $$("[name='comment']")[0];
+
+    // Require IE9+ and other supporting browsers.
+    if(!textarea || !("selectionStart" in textarea)) {
+      return;
+    }
+
+    var list, i, button, li, img, box = (window.pouetbox_bbspost || window.pouetbox_prodpost);
+    if (box)
+    {
+      list = document.createElement("ul");
+      list.style.top = textarea.offsetTop + "px";
+
+      for (i = 0; i < buttons.length; i++)
+      {
+        button = buttons[i];
+
+        li = document.createElement("li");
+        li.className = "bb_editor_" + (button["class"] || button.name.replace(/ /g, "_").toLowerCase());
+        li.title = button.name;
+        li.innerHTML = button.name;
+        li.onclick = function ()
+        {
+          (button.click || simpleEdit).call(button);
+        };
+
+        list.appendChild(li);
+      };
+
+      box.appendChild(list);
+      textarea.style.height = box.lastChild.offsetHeight + "px"; // purely aesthetics
+    }
+  });
+}());
+
