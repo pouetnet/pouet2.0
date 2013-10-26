6d7a532435489576a58f701177df6072a3311a05
Gargaj <gargaj@conspiracy.hu>
2013-10-26 19:00:53 +0200

    typo :(

--- user.php
+++ user.php
@@ -220,7 +220,7 @@ class PouetBoxUserMain extends PouetBox
   function GetNFOsAdded( $limit = null )
   {
     $s = new BM_Query("prods");
-    $s->AddOrder("nfos.added.desc");
+    $s->AddOrder("nfos.added desc");
     $s->AddJoin("left","nfos","prods.id = nfos.prod");
     $s->AddWhere(sprintf("nfos.user = %d",$this->id));
     if ($limit)
