3f0f6a60f6eac632a37c6ad963c13438ab294796
Gargaj <gargaj@conspiracy.hu>
2014-01-03 16:54:45 +0100

    Enclosure must be full URL apparently

--- export/lastprodsreleased.rss.php
+++ export/lastprodsreleased.rss.php
@@ -40,7 +40,7 @@ foreach($data as $item)
     "title"     => $item->name,
     "link"      => POUET_ROOT_URL . "prod.php?which=" . $item->id,
     "pubDate"   => date("r",strtotime($item->quand)),
-    "enclosure" => find_screenshot($item->id),
+    "enclosure" => POUET_CONTENT_URL . find_screenshot($item->id),
   ));
 }
 
