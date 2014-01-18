fe64698d935e6cde697e868a6f8e27361478b36a
Gargaj <gargaj@conspiracy.hu>
2014-01-18 15:43:34 +0100

    Add strikeout

--- include_generic/libbb.php
+++ include_generic/libbb.php
@@ -103,6 +103,7 @@ function bbencode( $text )
   $text = preg_replace("/\[b\](.*?)\[\/b\]/si","<b>$1</b>",$text);
   $text = preg_replace("/\[i\](.*?)\[\/i\]/si","<i>$1</i>",$text);
   $text = preg_replace("/\[u\](.*?)\[\/u\]/si","<u>$1</u>",$text);
+  $text = preg_replace("/\[s\](.*?)\[\/s\]/si","<s>$1</s>",$text);
   $text = bbencode_parse_tag($text,"quote","<section class=\"bbs_quote\"><b>Quote:</b><blockquote>","</blockquote></section>");
   $text = bbencode_parse_tag($text,"code","<section class=\"bbs_code\"><b>Code:</b><code>","</code></section>");
   $text = preg_replace("/\[list\](.*?)\[\/list\]/si","<ul>$1</ul>",$text);
