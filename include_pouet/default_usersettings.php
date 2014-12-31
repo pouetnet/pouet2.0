6c85494962a427f03b8143f4adfb05a38a78ae95
Gargaj <gargaj@conspiracy.hu>
2014-12-31 15:43:21 +0100

    remove deprecated fields

--- include_pouet/default_usersettings.php
+++ include_pouet/default_usersettings.php
@@ -4,14 +4,6 @@
 //echo serialize($r);
 
 $TEST = array(
-   'indextopglops' => 10,
-   'indextopprods' => 10,
-   'indexoneliner' => 5,
-   'indexlatestadded' => 5,
-   'indexlatestreleased' => 5,
-   'indexojnews' => 5,
-   'indexlatestcomments' => 5,
-   'indexbbstopics' => 10,
    'topicposts' => 25,
    'bbsbbstopics' => 25,
    'prodlistprods' => 25,
@@ -26,10 +18,6 @@ $TEST = array(
    'userrulez' => 10,
    'usersucks' => 10,
    'commentshours' => 24,
-   'indexcdc' => 1,
-   'indexsearch' => 1,
-   'indexlinks' => 1,
-   'indexstats' => 1,
    'logos' => 1,
    'topbar' => 1,
    'bottombar' => 1,
@@ -40,13 +28,14 @@ $TEST = array(
    'indexplatform' => 1,
    'indexwhoaddedprods' => 0,
    'indexwhocommentedprods' => 0,
-   'indexlatestparties' => 5,
-   'indextopkeops' => 10,
    'displayimages' => 1,
-   'indexbbsnoresidue' => 1,
    'prodcomments' => -1,
-   'indexwatchlist' => 5,
-   'customizerJSON' => '{"frontpage":{"leftbar":[{"box":"Login"},{"box":"CDC","limit":"1"},{"box":"LatestAdded","limit":"5"},{"box":"LatestReleased","limit":"5"},{"box":"TopMonth","limit":"10"},{"box":"TopAlltime","limit":"10"}],"middlebar":[{"box":"LatestOneliner","limit":"5"},{"box":"LatestBBS","limit":"10","hideResidue":true},{"box":"NewsBoxes","limit":"5"}],"rightbar":[{"box":"SearchBox","limit":"1"},{"box":"Stats","limit":"1"},{"box":"AffilButton","limit":"1"},{"box":"LatestComments","limit":"5"},{"box":"Watchlist","limit":"5"},{"box":"LatestParties","limit":"5"},{"box":"UpcomingParties"},{"box":"TopGlops","limit":"10"}]}}',
+   'customizerJSON' => 
+    '{"frontpage":{'.
+      '"leftbar":[{"box":"Login"},{"box":"CDC","limit":"1"},{"box":"LatestAdded","limit":"5"},{"box":"LatestReleased","limit":"5"},{"box":"TopMonth","limit":"10"},{"box":"TopAlltime","limit":"10"}],'.
+      '"middlebar":[{"box":"LatestOneliner","limit":"5"},{"box":"LatestBBS","limit":"10","hideResidue":true},{"box":"NewsBoxes","limit":"5"}],'.
+      '"rightbar":[{"box":"SearchBox","limit":"1"},{"box":"Stats","limit":"1"},{"box":"AffilButton","limit":"1"},{"box":"LatestComments","limit":"5"},{"box":"Watchlist","limit":"5"},{"box":"LatestParties","limit":"5"},{"box":"UpcomingParties"},{"box":"TopGlops","limit":"10"}]'.
+    '}}',
 );
 $DEFAULT_USERSETTINGS = new stdClass();
 foreach($TEST as $k=>$v) $DEFAULT_USERSETTINGS->$k = $v;
