ed620df34076fed1a20c58ca48bc0f91a108f8c5
Gargaj <gargaj@conspiracy.hu>
2015-01-01 16:29:12 +0100

    add protocol

--- cron.php
+++ cron.php
@@ -36,6 +36,9 @@ function cron_CheckLinks()
     
     $a = array();
     $a["prodID"] = $prod->id;
+    $a["protocol"] = "http";
+    if (strpos($prod->download,"ftp://")===0)
+      $a["protocol"] = "ftp";
     $a["testDate"] = date("Y-m-d H:i:s");
     $a["returnCode"] = curl_getinfo($ch,CURLINFO_HTTP_CODE);
     $a["returnContentType"] = curl_getinfo($ch,CURLINFO_CONTENT_TYPE);
