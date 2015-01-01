bd1139de7bd2617d05a0564e3f2199007dafb9c6
Gargaj <gargaj@conspiracy.hu>
2015-01-01 15:59:34 +0100

    add cron to link-check

--- cron.php
+++ cron.php
@@ -10,6 +10,45 @@ require_once("admin.functions.php");
 
 echo "[".date("Y-m-d H:i:s")."] Cron running: ".$argv[1]."\n";
 
+function cron_CheckLinks()
+{
+  $s = new SQLSelect();
+  $s->AddField("prods.id");
+  $s->AddField("prods.download");
+  $s->AddTable("prods");
+  $s->AddJoin("left","prods_linkcheck","prods_linkcheck.prodID = prods.id");
+  $s->AddWhere("prods_linkcheck.testDate is NULL or datediff(now(),prods_linkcheck.testDate) > 30");
+  $s->AddOrder("RAND()");
+  $s->SetLimit(5);
+  $prods = SQLLib::SelectRows( $s->GetQuery() );
+  $out = "";
+  foreach($prods as $prod)
+  {
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $prod->download);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+    curl_setopt($ch, CURLOPT_USERAGENT, "Pouet-BrokenLinkCheck/2.0");
+    curl_setopt($ch, CURLOPT_NOBODY, true);    
+    
+    curl_exec($ch);
+    
+    $a = array();
+    $a["prodID"] = $prod->id;
+    $a["testDate"] = date("Y-m-d H:i:s");
+    $a["returnCode"] = curl_getinfo($ch,CURLINFO_HTTP_CODE);
+    $a["returnContentType"] = curl_getinfo($ch,CURLINFO_CONTENT_TYPE);
+    SQLLib::UpdateOrInsertRow("prods_linkcheck",$a,sprintf_esc("prodID=%d",$prod->id));
+    
+    curl_close($ch);
+    
+    $out .= "\n[".$prod->id."] " . $prod->download . " >> ". $a["returnCode"];
+    sleep(5);
+  }
+  return $out;
+}
+
 switch($argv[1])
 {
   case "recacheTopDemos":
@@ -17,6 +56,10 @@ switch($argv[1])
     preg_match_all("/<h3>(.*)<\/h3>/",$content,$m);
     foreach($m[1] as $v)
       echo " > Recache: ".$v."\n";
+    break;
+  case "linkCheck":
+    $content = cron_CheckLinks();
+    echo " > linkCheck: ".$content."\n";
     break;
 }
 
