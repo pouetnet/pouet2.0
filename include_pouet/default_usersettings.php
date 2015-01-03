fb22c4798acd97fa65df186720ee12c92fc4c5bb
Gargaj <gargaj@conspiracy.hu>
2015-01-03 23:23:50 +0100

    odd indentation

--- include_pouet/default_usersettings.php
+++ include_pouet/default_usersettings.php
@@ -1,40 +1,36 @@
 <?
-
-//$r = SQLLib::SelectRow("select * from usersettings where id = 11057");
-//echo serialize($r);
-
 $DEFAULT_USERSETTINGS = (object)array(
-   'topicposts' => 25,
-   'bbsbbstopics' => 25,
-   'prodlistprods' => 25,
-   'searchprods' => 25,
-   'userlogos' => 10,
-   'userprods' => 10,
-   'usergroups' => 10,
-   'userparties' => 10,
-   'userscreenshots' => 10,
-   'usernfos' => 10,
-   'usercomments' => 10,
-   'userrulez' => 10,
-   'usersucks' => 10,
-   'commentshours' => 24,
-   'logos' => 1,
-   'topbar' => 1,
-   'bottombar' => 1,
-   'userlistusers' => 25,
-   'topichidefakeuser' => 0,
-   'prodhidefakeuser' => 0,
-   'indextype' => 1,
-   'indexplatform' => 1,
-   'indexwhoaddedprods' => 0,
-   'indexwhocommentedprods' => 0,
-   'displayimages' => 1,
-   'prodcomments' => -1,
-   'customizerJSON' => 
-    '{"frontpage":{'.
-      '"leftbar":[{"box":"Login"},{"box":"CDC","limit":"1"},{"box":"LatestAdded","limit":"5"},{"box":"LatestReleased","limit":"5"},{"box":"TopMonth","limit":"10"},{"box":"TopAlltime","limit":"10"}],'.
-      '"middlebar":[{"box":"LatestOneliner","limit":"5"},{"box":"LatestBBS","limit":"10","hideResidue":true},{"box":"NewsBoxes","limit":"5"}],'.
-      '"rightbar":[{"box":"SearchBox","limit":"1"},{"box":"Stats","limit":"1"},{"box":"AffilButton","limit":"1"},{"box":"LatestComments","limit":"5"},{"box":"Watchlist","limit":"5"},{"box":"LatestParties","limit":"5"},{"box":"UpcomingParties"},{"box":"TopGlops","limit":"10"}]'.
-    '}}',
+  'topicposts' => 25,
+  'bbsbbstopics' => 25,
+  'prodlistprods' => 25,
+  'searchprods' => 25,
+  'userlogos' => 10,
+  'userprods' => 10,
+  'usergroups' => 10,
+  'userparties' => 10,
+  'userscreenshots' => 10,
+  'usernfos' => 10,
+  'usercomments' => 10,
+  'userrulez' => 10,
+  'usersucks' => 10,
+  'commentshours' => 24,
+  'logos' => 1,
+  'topbar' => 1,
+  'bottombar' => 1,
+  'userlistusers' => 25,
+  'topichidefakeuser' => 0,
+  'prodhidefakeuser' => 0,
+  'indextype' => 1,
+  'indexplatform' => 1,
+  'indexwhoaddedprods' => 0,
+  'indexwhocommentedprods' => 0,
+  'displayimages' => 1,
+  'prodcomments' => -1,
+  'customizerJSON' => 
+  '{"frontpage":{'.
+    '"leftbar":[{"box":"Login"},{"box":"CDC","limit":"1"},{"box":"LatestAdded","limit":"5"},{"box":"LatestReleased","limit":"5"},{"box":"TopMonth","limit":"10"},{"box":"TopAlltime","limit":"10"}],'.
+    '"middlebar":[{"box":"LatestOneliner","limit":"5"},{"box":"LatestBBS","limit":"10","hideResidue":true},{"box":"NewsBoxes","limit":"5"}],'.
+    '"rightbar":[{"box":"SearchBox","limit":"1"},{"box":"Stats","limit":"1"},{"box":"AffilButton","limit":"1"},{"box":"LatestComments","limit":"5"},{"box":"Watchlist","limit":"5"},{"box":"LatestParties","limit":"5"},{"box":"UpcomingParties"},{"box":"TopGlops","limit":"10"}]'.
+  '}}',
 );
 ?>
