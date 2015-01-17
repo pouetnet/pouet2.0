8325683c206de1b87fb61c03a39a3feb037fb4d0
Gargaj <gargaj@conspiracy.hu>
2015-01-17 18:56:02 +0100

    add wanted on default front page

--- include_pouet/default_usersettings.php
+++ include_pouet/default_usersettings.php
@@ -30,7 +30,7 @@ $DEFAULT_USERSETTINGS = (object)array(
   '{"frontpage":{'.
     '"leftbar":[{"box":"Login"},{"box":"CDC","limit":"1"},{"box":"LatestAdded","limit":"5"},{"box":"LatestReleased","limit":"5"},{"box":"TopMonth","limit":"10"},{"box":"TopAlltime","limit":"10"}],'.
     '"middlebar":[{"box":"LatestOneliner","limit":"5"},{"box":"LatestBBS","limit":"10","hideResidue":true},{"box":"NewsBoxes","limit":"5"}],'.
-    '"rightbar":[{"box":"SearchBox","limit":"1"},{"box":"Stats","limit":"1"},{"box":"AffilButton","limit":"1"},{"box":"LatestComments","limit":"5"},{"box":"Watchlist","limit":"5"},{"box":"LatestParties","limit":"5"},{"box":"UpcomingParties"},{"box":"TopGlops","limit":"10"}]'.
+    '"rightbar":[{"box":"SearchBox","limit":"1"},{"box":"Stats","limit":"1"},{"box":"AffilButton","limit":"1"},{"box":"LatestComments","limit":"5"},{"box":"Watchlist","limit":"5"},{"box":"LatestParties","limit":"5"},{"box":"UpcomingParties"},{"box":"FeedWanted"},{"box":"TopGlops","limit":"10"}]'.
   '}}',
 );
 ?>
