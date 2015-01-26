e9f5f7ed077bed5f9478b8eed85dda8d15d45e52
Gargaj <gargaj@conspiracy.hu>
2015-01-26 23:00:24 +0100

    edit twitter feed to use the official api

--- include_pouet_index/box-index-feed-pouettwitter.php
+++ include_pouet_index/box-index-feed-pouettwitter.php
@@ -38,23 +38,56 @@ class PouetBoxIndexFeedPouetTwitter extends PouetBoxCachable {
   }
 
   function LoadFromCachedData($data) {
-    $this->rssData = unserialize($data);
+    $this->jsonData = unserialize($data);
   }
 
   function GetCacheableData() {
-    return serialize($this->rssData);
+    return serialize($this->jsonData);
   }
 
-  function LoadFromDB() {
-    $this->rssData = $this->rss->get('http://www.queryfeed.net/twitter?q=from%3Apouetdotnet');
+  function Sideload( $url, $data = array(), $headers = array(), $method = "GET" )
+  {
+    if ($method == "GET")
+      $url .= (strstr($url,"?") === false ? "?" : "&") . http_build_query($data);
+    return file_get_contents( $url, false, stream_context_create( array(
+      'http'=>array(
+        'method'=>$method,
+        'header'=>implode("\r\n",$headers),
+        'content'=>($method == "GET" ? null : http_build_query($data)),
+      ),
+      'ssl' => array(
+        'verify_peer' => false,
+      ),
+    ) ) );
+  }
+  function LoadFromDB() 
+  {
+    if (!defined("TWITTER_CONSUMER_KEY")) return;
+    
+    $auth = "Basic " . base64_encode( TWITTER_CONSUMER_KEY . ":" . TWITTER_CONSUMER_SECRET );
+    
+    $authTokens = json_decode( $this->Sideload( "https://api.twitter.com/oauth2/token", array("grant_type"=>"client_credentials"), array("Authorization: ".$auth), "POST" ) );
+    if (!$authTokens || !$authTokens->access_token)
+      return;
+    $auth2 = "Bearer ".$authTokens->access_token;
+    
+    // doc @ https://dev.twitter.com/rest/reference/get/statuses/user_timeline
+  
+    $statuses = array();
+    
+    $data = json_decode( $this->Sideload( "https://api.twitter.com/1.1/statuses/user_timeline.json", array("screen_name"=>"pouetdotnet","count"=>10), array("Authorization: ".$auth2) ) );
+    if (!$data || !is_array($data))
+      return;
+    
+    $this->jsonData = $data;
   }
 
   function RenderBody() {
     echo "<ul class='boxlist'>\n";
-    for($i=0; $i < min( count($this->rssData['items']),$this->limit); $i++)
+    for($i=0; $i < min( count($this->jsonData),$this->limit); $i++)
     {
       echo "<li>\n";
-      echo "<a href='".$this->rssData['items'][$i]['link']."'>".strip_tags($this->rssData['items'][$i]['description'])."</a> ";
+      echo "<a href='https://twitter.com/pouetdotnet/status/"._html($this->jsonData[$i]->id_str)."'>"._html(strip_tags($this->jsonData[$i]->text))."</a> ";
       echo "</li>\n";
     }
     echo "</ul>\n";
