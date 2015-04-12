a3ae44d5d276afc934adb63aceea48b871a87a59
Gargaj <gargaj@conspiracy.hu>
2015-04-12 15:24:48 +0200

    fix #54

--- oneliner.php
+++ oneliner.php
@@ -31,7 +31,7 @@ class PouetBoxOnelinerView extends PouetBox {
     //$s->SetLimit( $POSTS_PER_PAGE, (int)(($this->page - 1)*$POSTS_PER_PAGE) );
 
     $this->paginator = new PouetPaginator();
-    $this->paginator->SetData( "oneliner.php", $this->postcount, $POSTS_PER_PAGE, $_GET["page"] );
+    $this->paginator->SetData( ($_GET["who"] ? "oneliner.php?who=".(int)$_GET["who"] : "oneliner.php"), $this->postcount, $POSTS_PER_PAGE, $_GET["page"] );
     $this->paginator->SetLimitOnQuery( $s );
 
     $this->oneliner = $s->perform();
