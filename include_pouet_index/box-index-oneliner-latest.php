d499720c929f9822d0f7220fbb51d7faf231a294
Gargaj <gargaj@conspiracy.hu>
2018-10-08 17:37:04 +0200

    giggles

--- include_pouet_index/box-index-oneliner-latest.php
+++ include_pouet_index/box-index-oneliner-latest.php
@@ -135,7 +135,8 @@ class PouetBoxIndexLatestOneliner extends PouetBoxCachable {
       //$funnytext = "time is to unicode on the onliner";
       //$funnytext = "pou\303\253t 2.0: \303\274nic\303\270de \320\270ow \321\210\305\221rks in the \317\203neli\316\256er";
       //$funnytext = "that moment when you accidentally mistyped demozoo";
-      $funnytext = "I am actually more of a rebel than ever.";
+      //$funnytext = "I am actually more of a rebel than ever.";
+      $funnytext = "But seriously.";
 
       echo "  <div class='foot loggedin'>\n";
       echo "   <span><a href='oneliner.php'>more</a>...</span>\n";
