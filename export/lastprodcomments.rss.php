9887272304b130fef628533b9cfc182eb296c5bd
Gargaj <gargaj@conspiracy.hu>
2022-11-27 00:41:46 +0100

    fix rss when there are no comments

--- export/lastprodcomments.rss.php
+++ export/lastprodcomments.rss.php
@@ -6,24 +6,23 @@ $limit = $_GET["howmany"] ? (int)$_GET["howmany"] : 10;
 $limit = min($limit,25);
 $limit = max($limit,5);
 
+$prod = PouetProd::Spawn( $_GET["prod"] );
+if (!$prod) exit();
+
 $s = new BM_Query("comments");
 $s->AddField("comments.id");
 $s->AddField("comments.addedDate");
 $s->AddField("comments.comment");
 $s->AddField("comments.rating");
-$s->attach(array("comments"=>"which"),array("prods as prod"=>"id"));
 $s->attach(array("comments"=>"who"),array("users as user"=>"id"));
 $s->AddWhere(sprintf_esc("comments.which = %d",$_GET["prod"]));
 $s->AddOrder("comments.addedDate DESC");
 $s->SetLimit($limit);
 $data = $s->perform();
 
-$first = reset($data);
-if (!$first || !$first->prod) exit();
-
 $rss = new PouetRSS(array(
-  "title"=>"pouët.net - prod comments for ".$first->prod->name,
-  "link"=>$first->prod->GetLink(),
+  "title"=>"pouët.net - prod comments for ".$prod->name,
+  "link"=>$prod->GetLink(),
 ));
 
 $votes = array( -1 => "sucks", 0 => "isok", 1 => "rulez" );
