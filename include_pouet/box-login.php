8f0d84ebde7ce79b98699d0809b77402905bad5f
Gargaj <gargaj@conspiracy.hu>
2023-11-01 09:57:07 +0100

    fix ambiguous login methods

--- include_pouet/box-login.php
+++ include_pouet/box-login.php
@@ -4,24 +4,26 @@ require_once( POUET_ROOT_LOCAL . "/include_pouet/pouet-box.php");
 require_once( POUET_ROOT_LOCAL . "/include_pouet/pouet-prod.php");
 require_once( POUET_ROOT_LOCAL . "/include_pouet/pouet-user.php");
 
-class PouetBoxLogin extends PouetBox 
+class PouetBoxLogin extends PouetBox
 {
   var $data;
   var $prod;
-  function __construct() 
+  function __construct()
   {
     parent::__construct();
     $this->uniqueID = "pouetbox_login";
     $this->title = "login";
   }
-  function RenderBody() 
+  function RenderBody()
   {
+    $loginURL = "login.php?return=".rawurlencode(rootRelativePath());
+
     echo "<div class='content loggedout'>\n";
-    printf( "<a href='login.php?return=%s'>login via SceneID</a>",_html(rawurlencode(rootRelativePath())) );
+    printf( "<a href='%s'>login via SceneID</a>",_html($loginURL) );
     echo "</div>\n";
 
     echo "<div class='foot'>\n";
-    echo "<a href='https://id.scene.org/login/'>log in</a> ::\n";
+    echo "<a href='"._html($loginURL)."'>log in</a> ::\n";
     echo "<a href='https://id.scene.org/register/'>register</a>\n";
     echo "</div>";
   }
