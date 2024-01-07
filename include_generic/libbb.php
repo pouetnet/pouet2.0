b22a3b4d43b6de138503de7aa4b3a2971788c9be
Gargaj <gargaj@conspiracy.hu>
2024-01-07 21:15:56 +0100

    PHP8 etc etc

--- include_generic/libbb.php
+++ include_generic/libbb.php
@@ -118,7 +118,7 @@ function bbencode( $text )
   $text = preg_replace("/\[url=(.*?)\](.*?)\[\/url\]/si","<a href='http://$1'>$2</a>",$text);
   
   $text = preg_replace("/\[email\](.*?)\[\/email\]/si","<a href='mailto:$1'>$1</a>",$text);
-  if (get_setting("displayimages") || $_GET["forceimages"])
+  if (get_setting("displayimages") || @$_GET["forceimages"])
     $text = preg_replace("/\[img\](.*?)\[\/img\]/i","<img src='$1' loading='lazy' class='bbimage' alt='BB Image'/>",$text);
   else
     $text = preg_replace("/\[img\](.*?)\[\/img\]/i","<a href='$1'>[image disabled]</a>",$text);
